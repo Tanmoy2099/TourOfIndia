f5608365aae9406866008103a1f5ed544b287bed
Tanmoy2099 <87271422+Tanmoy2099@users.noreply.github.com>
2022-07-22 20:04:56 +0530

    Procfile added

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-    console.log(err, err.name, err.message);
-    process.exit(1);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err, err.name, err.message);
+  process.exit(1);
 });
 
 
@@ -17,23 +17,23 @@ dotenv.config({ path: './.env' });
 
 
 const DB_URL = process.env.DATABASE.replace('<username>', process.env.USERNAME_DB)
-    .replace('<password>', process.env.PASSWORD_DB)
+  .replace('<password>', process.env.PASSWORD_DB)
 
 async function main() {
-    await mongoose.connect(DB_URL);
+  await mongoose.connect(DB_URL);
 }
 
 const server = app.listen(port, () => console.log(`Server is running at port: ${port}`))
 
 main()
-    .then(console.log("Successfully connected to Database!"))
-    .catch((err) => {
-        server.close(() => process.exit(1));
-        console.log(err, `Could not connect to Database, ${err.message}`)
-    });
+  .then(console.log("Successfully connected to Database!"))
+  .catch((err) => {
+    server.close(() => process.exit(1));
+    console.log(err, `Could not connect to Database, ${err.message}`)
+  });
 
 process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.log(err, err.name, err.message);
-    server.close(() => process.exit(1));
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err, err.name, err.message);
+  server.close(() => process.exit(1));
 });
